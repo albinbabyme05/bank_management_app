5d147bdbb3dc3ec8b209752e4a32511e89855276
Albin Baby <albinbabayme05@gmail.com>
2025-01-01 20:01:14 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<cstdlib>
+#include<fstream>
+
+using namespace std;
+
+class Bank{
+private:
+
+
+public:
+
+
+void functionName() {
+
+
+}
+};
+
+
+void display(){
+    cout<<""<<endl;
+    cout<<"1. Create a New Account"<<endl;
+    cout<<"2. View Account Details"<<endl;
+    cout<<"3. Deposit Money"<<endl;
+    cout<<"4. Withdraw Money"<<endl;
+    cout<<"5. Close Account"<<endl;
+    cout<<"6. Exit"<<endl;
+}
+
+
+int main() {
+    
+
+
+
+    return 0;
+}
