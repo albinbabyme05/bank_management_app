6b1900d7f5dd0c51f1cf5248fa11df66170764cf
Albin Baby <albinbabayme05@gmail.com>
2025-01-02 09:54:28 +0100

    account creation completed and unique account number given to new users

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 #include<cstdlib>
 #include<fstream>
+#include <vector>
 
 using namespace std;
 
 class Bank{
 private:
+    int deposit;
+    string name;
+    string accountNumber = "1001" + to_string(rand()% 9000 + 10000);;
 
 
 public:
+    Bank(string accountHolder, int amount ){
+        name = accountHolder;
+        deposit = amount;
+        accountNumber =  accountNumber;
+    }
 
+    string getAccountHolder(){
+        return name;
+    }
 
-void functionName() {
+    int getBalance(){
+        return deposit;
+    }
 
+    string getAccountNumber(){
+        return accountNumber;
+    }
 
-}
+    string setName(string newAccountHolder){
+        return name = newAccountHolder;
+    }
+
+    int setBalance(int newAmount){
+        return deposit = newAmount;
+    }
+
+    void display(){
+        cout<<"name : "<<name<<endl;
+        cout<<"balance : "<<deposit<<endl;
+        cout<<"Account number : "<<accountNumber<<endl;
+    }
+
+    
+
 };
 
 
@@ -30,9 +62,76 @@ void display(){
 
 
 int main() {
+
+    vector<Bank> userDetails;
+
+
+    int choice;
+    string userName, option;
+    int userDeposit;
+
     
 
+    
+    do
+    {
+    display();
 
+    cout<<"Enter your choice ";
+    cin>>choice;
+
+    cout<<"Enter new User name : "<<endl;
+	cin>>userName;
+	cout<<"Enter initial amount: "<<endl;
+	cin>>userDeposit;
+
+
+
+    Bank newUser(userName, userDeposit);
+   
+    userDetails.push_back(newUser);
+    
+
+     switch (choice)
+    {
+    case 1:
+        for (auto details : userDetails) 
+        details.display();
+        break;
+
+    // case 2:
+        
+    //     break;
+
+    // case 3:
+        
+    //     break;
+
+    // case 4:
+        
+    //     break;
+
+    // case 5:
+        
+    //     break;
+
+    // case 6:
+        
+    //     break;
+
+    default:
+        break;
+    }
+
+    cout<<"Do you wnat to continue ? press 'y/Y' to continue... ";
+	cin>>option;
+
+   } while (option=="y" || option =="Y");
+    
+
+    
+
+   
 
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+case 2:
+        
+    //     break;
+
+    // case 3:
+        
+    //     break;
+
+    // case 4:
+        
+    //     break;
+
+    // case 5:
+        
+    //     break;
+
+    // case 6:
+        
+    //     break;
